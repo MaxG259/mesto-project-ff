33fae2f95f13481f7bbe3085de8ae167aaed1745
Maks Vovk <maksvovk259@yandex.ru>
2025-04-07 23:40:21 +0400

    first public

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+//Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+//DOM узлы
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+//Функция создания карточки
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteCard(cardElement);
+  });
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+
+
+//Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
+}
+
+
+
+//Вывести карточки на страницу
+initialCards.forEach(function(cardData) {
+  const newCard = createCard(cardData, deleteCard);
+  placesList.append(newCard);
+});
